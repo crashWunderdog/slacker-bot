22fbd13dbce36de2422225806e67efdeb5bc4259
Sami Ollila <sami.ollila@wunderdog.fi>
2025-03-04 16:27:02 +0100

    fix: small clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { CirclePacking } from "./components/CirclePacking";
 import { countMessages } from "./lib/messageCounter";
 
@@ -8,12 +9,26 @@ export interface DataNode extends d3.SimulationNodeDatum {
 }
 
 export default async function Home() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <HomeScreen />
+    </Suspense>
+  );
+}
+
+const Loading = () => (
+  <div className="flex justify-center items-center h-screen w-screen">
+    <p className="text-white text-xl animate-pulse">Loading...</p>
+  </div>
+);
+
+async function HomeScreen() {
   const data = await countMessages();
 
   if (!data) {
     return (
       <div className="flex justify-center items-center h-screen w-screen">
-        <p className="text-white text-xl">Loading...</p>
+        <p className="text-white text-xl">Loading data failed :(</p>
       </div>
     );
   }
@@ -29,7 +44,7 @@ const DataRenderer = ({ data }: { data: DataNode[] }) => {
         <h1 className="text-2xl text-center pb-4">MVPs</h1>
         <ul className="pl-5">
           {data.toReversed().map((item, index) => (
-            <li key={index} className="text-lg">
+            <li key={item.group} className="text-lg">
               {++index}.{item.group}: {item.value}
             </li>
           ))}
